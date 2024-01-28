3c0b0e207ee806722afde7c0676f8546045e528a
Abrar <209x1a05g2@gprec.ac.in>
2024-01-28 14:23:47 +0530

    student class added

--- Railway-demo/src/main/java/com/example/Railway/demo/Controllers/Studentcontroller.java
+++ Railway-demo/src/main/java/com/example/Railway/demo/Controllers/Studentcontroller.java
@@ -1,15 +1,22 @@
 package com.example.Railway.demo.Controllers;
 
+import com.example.Railway.demo.Models.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class Studentcontroller {
 
     @GetMapping("/students")
-    public String getAll()
+    public List<Student> getAll()
     {
-        return "Hello All Students !";
+        List<Student> studs= Arrays.asList(new Student(10,"Abrar","209x1a05g2"),
+                new Student(11,"Abdul","209x1a05g1"));
+        return studs;
+
     }
 
     @GetMapping("/")
---
+++ Railway-demo/src/main/java/com/example/Railway/demo/Models/Student.java
@@ -0,0 +1,16 @@
+package com.example.Railway.demo.Models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Student {
+
+    int id;
+    String name;
+    String rollno;
+}
